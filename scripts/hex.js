da63b86c8832a1ef77fca37f3ea8477df08edff5
Mohan54001mo <mohanbharathi542001@gmail.com>
2023-02-07 09:57:26 +0530

    Initial commit

---
+++ scripts/hex.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// creating selecting elements
+const btnEl = document.getElementById('btn');
+
+const color = document.querySelector('.colors');
+
+// global varibales
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+// functions
+// random number function
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
+// add eventlistener
+
+btnEl.addEventListener('click', function () {
+  console.log('hii');
+  let hexColor = '#';
+
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+
+  // color.textContent = colors[randomNumber];
+});
---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// creating selecting elements
+const btnEl = document.getElementById('btn');
+
+const color = document.querySelector('.colors');
+
+// global varibales
+const colors = ['red', 'green', 'rgba(133,122,200)', '#f15025'];
+// functions
+// random number function
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
+// add eventlistener
+
+btnEl.addEventListener('click', function () {
+  console.log('hii');
+  // creating randomNumber
+  const randomNumber = getRandomNumber();
+
+  document.body.style.backgroundColor = colors[randomNumber];
+
+  color.textContent = colors[randomNumber];
+});
